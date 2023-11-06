b8476c341b35109cb706a56d62d3da2b15c06daf
CruzPy <hansellcr25@gmail.com>
2023-11-06 18:52:12 -0500

    Modified file structure

---
+++ EnochRecoveryApp/static/js/index.js
@@ -0,0 +1,96 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    //... and fix the Previous/Next buttons:
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 1)) {
+        document.getElementById("nextBtn").innerHTML = "Submit";
+    } else {
+        document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    //... and run a function that will display the correct step indicator:
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tab");
+    // Exit the function if any field in the current tab is invalid:
+    if (n == 1 && !validateForm()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form...
+    if (currentTab >= x.length) {
+        // ... the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+}
+
+function validateForm() {
+    // This function deals with validation of the form fields
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+            // add an "invalid" class to the field:
+            y[i].className += " invalid";
+            // and set the current valid status to false
+            valid = false;
+        }
+    }
+    // If the valid status is true, mark the step as finished and valid:
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; // return the valid status
+}
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class on the current step:
+    x[n].className += " active";
+}
+
+// This function will set the default date to today and the default time to the next hour
+function setDefaultDateTime() {
+    var today = new Date();
+    var dateInput = document.getElementById("date");
+    var timeSelect = document.getElementById("time");
+
+    // Set the date input value to today's date
+    dateInput.value = today.toISOString().slice(0, 10);
+
+    // Calculate the next hour
+    var nextHour = today.getHours() + 1;
+    if (nextHour > 17) { // Check if it's past 5 PM, then set to 9 AM next day
+        nextHour = 9;
+        dateInput.value = new Date(today.getTime() + 24 * 60 * 60 * 1000).toISOString().slice(0, 10); // Increment the date by one day
+    }
+
+    // Set the selected option in the time select element
+    var nextHourOption = timeSelect.querySelector('option[value="' + ('0' + nextHour).slice(-2) + ':00' + '"]');
+    nextHourOption.selected = true;
+}
+
+// Call the function to set the default values
+setDefaultDateTime();
