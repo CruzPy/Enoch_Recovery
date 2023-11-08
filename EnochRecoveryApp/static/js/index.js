9311cb764ab04fa0a2dd44c9afa7f348b8fb9c61
CruzPy <hansellcr25@gmail.com>
2023-11-07 19:46:44 -0500

    Added cycling images Finalized form Submittal page updated Video finalized Faces blurred

--- EnochRecoveryApp/static/js/index.js
+++ EnochRecoveryApp/static/js/index.js
@@ -17,7 +17,7 @@ function showTab(n) {
         document.getElementById("nextBtn").innerHTML = "Next";
     }
     //... and run a function that will display the correct step indicator:
-    fixStepIndicator(n)
+    fixStepIndicator(n);
 }
 
 function nextPrev(n) {
@@ -71,26 +71,149 @@ function fixStepIndicator(n) {
     x[n].className += " active";
 }
 
-// This function will set the default date to today and the default time to the next hour
+// Modify the function to populate time slots based on the schedule
 function setDefaultDateTime() {
-    var today = new Date();
     var dateInput = document.getElementById("date");
     var timeSelect = document.getElementById("time");
 
-    // Set the date input value to today's date
-    dateInput.value = today.toISOString().slice(0, 10);
+    // Check if a date is selected
+    if (dateInput.value) {
+        var selectedDate = new Date(dateInput.value);
+        var selectedDayOfWeek = selectedDate.getDay();
 
-    // Calculate the next hour
-    var nextHour = today.getHours() + 1;
-    if (nextHour > 17) { // Check if it's past 5 PM, then set to 9 AM next day
-        nextHour = 9;
-        dateInput.value = new Date(today.getTime() + 24 * 60 * 60 * 1000).toISOString().slice(0, 10); // Increment the date by one day
+        var timeSlots = {
+            0: ["5:30 PM", "6:00 PM", "6:30 PM"],
+            1: ["5:30 PM", "6:00 PM", "6:30 PM"],
+            2: ["5:30 PM", "6:00 PM", "6:30 PM"],
+            5: ["10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM"],
+            6: ["2:30 PM", "3:00 PM", "3:30 PM"],
+        };
+
+        // Remove existing time options
+        timeSelect.innerHTML = "";
+
+        // Add the available time slots for the selected day
+        if (timeSlots[selectedDayOfWeek]) {
+            timeSlots[selectedDayOfWeek].forEach(function (time) {
+                var option = document.createElement("option");
+                option.value = time;
+                option.textContent = time;
+                timeSelect.appendChild(option);
+            });
+        }
+    } else {
+        // If no date is selected, clear the time options
+        timeSelect.innerHTML = "";
     }
-
-    // Set the selected option in the time select element
-    var nextHourOption = timeSelect.querySelector('option[value="' + ('0' + nextHour).slice(-2) + ':00' + '"]');
-    nextHourOption.selected = true;
 }
 
-// Call the function to set the default values
+// Modify the function to set the default date to today when the page is loaded
+function setDefaultDate() {
+    var dateInput = document.getElementById("date");
+
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1; // Months are 0-indexed, so add 1
+    var day = today.getDate();
+
+    // Format the date as "YYYY-MM-DD"
+    var formattedDate = year + '-' + (month < 10 ? '0' : '') + month + '-' + (day < 10 ? '0' : '') + day;
+
+    dateInput.value = formattedDate;
+}
+
+// Function to validate the date input
+function validateDate() {
+    var dateInput = document.getElementById("date");
+    var today = new Date();
+    var selectedDate = new Date(dateInput.value);
+
+    // Compare the selected date with the current date
+    if (selectedDate < today) {
+        if (selectedDate.toDateString() !== today.toDateString()) {
+            alert("Please select a date in the future.");
+            dateInput.value = ""; // Clear the date input
+        }
+    }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change slides every 5 seconds
+}
+
+function plusDivs(n) {
+    slideIndex += n;
+    var slides = document.getElementsByClassName("mySlides");
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    if (slideIndex < 1) {
+        slideIndex = slides.length;
+    }
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+
+// Call the function to set the default date when the page is loaded
+setDefaultDate();
+
+// Call the function to populate time slots based on the selected date
 setDefaultDateTime();
+
+// Add an event listener to the date input to update time options when the date changes
+document.getElementById("date").addEventListener("change", setDefaultDateTime);
+
+
+// Add an event listener to the date input to validate the date
+document.getElementById("date").addEventListener("change", validateDate);
+
+var shortText = document.getElementById('short-text');
+var fullText = document.getElementById('full-text');
+var msg = document.getElementById('msg');
+var readMoreLink = document.getElementById('read-more-link');
+var isExpanded = false;
+
+function toggleText() {
+    if (isExpanded) {
+        shortText.style.display = 'block';
+        msg.style.display = 'block'; // Show the "Message From Our Director" text
+        fullText.style.display = 'none';
+        readMoreLink.innerText = 'Read More';
+        isExpanded = false;
+    } else {
+        shortText.style.display = 'none';
+        fullText.style.display = 'block';
+        readMoreLink.innerText = 'Read Less';
+        msg.style.display = 'none'; // Hide the "Message From Our Director" text
+        isExpanded = true;
+    }
+}
+
+var videoContainer = document.getElementById('video-container');
+var iframe = videoContainer.querySelector('iframe');
+
+// Listen for a click event to expand the video
+videoContainer.addEventListener('click', function () {
+    if (iframe.src === '') {
+        // Load the video when clicked for the first time
+        iframe.src = 'https://drive.google.com/file/d/1A-pwWvWQlDmdfSzftqMuhB8KWUnu_rgk/preview';
+    }
+
+    // Expand the video to full size
+    videoContainer.style.width = '100%';
+});
--- EnochRecoveryApp/views.py
+++ EnochRecoveryApp/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render, redirect
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def submitted(request):
-    if request.method == 'POST':
-        first_name = request.POST.get('fname')
-        last_name = request.POST.get('lname')
-        email = request.POST.get('email')
-        phone = request.POST.get('phone')
-        date = request.POST.get('date')
-        time = request.POST.get('time')
-        choice = request.POST.get('choice')
+    if request.method == "POST":
+        first_name = request.POST.get("fname")
+        last_name = request.POST.get("lname")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        date = request.POST.get("date")
+        time = request.POST.get("time")
+        location = request.POST.get("location")
 
         context = {
-            'first_name': first_name,
-            'last_name': last_name,
-            'email': email,
-            'phone': phone,
-            'date': date,
-            'time': time,
-            'choice' : choice,
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "phone": phone,
+            "date": date,
+            "time": time,
+            "location": location,
         }
-        return render(request, 'submitted.html', context)
-    
+        return render(request, "submitted.html", context)
+
     else:
-        return redirect('home')  # Redirect to the home view if it's not a POST request
+        return redirect("home")  # Redirect to the home view if it's not a POST request
