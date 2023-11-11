0381034d2536d07fce42e05947680cb0ca1e32d6
CruzPy <hansellcr25@gmail.com>
2023-11-11 11:07:38 -0500

    Created template Seperated index and testimonials pages

--- EnochRecoveryApp/urls.py
+++ EnochRecoveryApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("testimonials/", views.testimonials, name="testimonials"),
     path("submitted/", views.submitted, name="submitted"),
 ]
--- EnochRecoveryApp/views.py
+++ EnochRecoveryApp/views.py
@@ -10,15 +10,20 @@ def home(request):
     }
     return render(request, "index.html", context)
 
+
+def testimonials(request):
+    return render(request, "testimonials.html")
+
+
 def submitted(request):
     if request.method == "POST":
         form = OrientationRequestForm(request.POST)
         if form.is_valid():
             form.save()  # Save to DB
-            
+
             return render(request, "submitted.html", {"form": form})
 
         else:
             print("Form is invalid")
-        
+
     return redirect("home")  # Redirect to the home view if it's not a POST request
